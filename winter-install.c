e356e2df1c8972cc2717b7607ca5a5a7a78cd2e0
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2017-06-05 02:24:50 -0300

    Update winter-install.c

--- winter-install.c
+++ winter-install.c
@@ -4,19 +4,20 @@
 #include "winter-lib.h"
 
 int main (){
-  char a[30], b[60], c[60];
+  short a; 
+  char b[60], c[60];
   strcpy(b, "git clone https://github.com/ShyanJMC/EkoLinux-repo/");
   system("clear");
   printf("Welcome, Do you wanna: \n 1-Install. \n 2-Run images.\n");
   scanf("%d", &a);
-  if (a = "1"){
+  if (a = 1){
   printf("\n Please type the dockerfile to install: " );
-  scanf("%s", &c)
+  scanf("%s", &c);
   strcat(b, c);
   system(b);
   docker(1, c); }
   else {
-    docker(2);
+    docker(2, c);
   }
   return 0;
 }

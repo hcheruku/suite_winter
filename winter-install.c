12e50af46478a311592dd0d5c2a4eaab160d2b8a
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2017-06-20 01:58:22 -0300

    Update and rewrite for some things

--- winter-install.c
+++ winter-install.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,19 +6,24 @@
 
 int main (){
   short a; 
-  char b[60], c[60];
-  strcpy(b, "git clone https://github.com/ShyanJMC/EkoLinux-repo/");
+  char c[30];
+  char b[60];
   system("clear");
   printf("Welcome, Do you wanna: \n 1-Install. \n 2-Run images.\n");
   scanf("%d", &a);
-  if (a = 1){
+  repository();
+  printf(" Se esta usando el repositorio: %s \n", Back.repository);
+  strcpy(b, Back.repository);
+  switch(a){
+    case 1: { 
   printf("\n Please type the dockerfile to install: " );
   scanf("%s", &c);
-  strcat(b, c);
-  system(b);
+  clone(c);
   docker(1, c); }
-  else {
-    docker(2, c);
+    case 2: {
+      docker(2, c);
+    }
+    default: printf(" Please type a valid option -_- \n");
   }
   return 0;
 }

9253f7e5473035b6a1cad3c9245675c3827f2f47
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2017-06-22 03:25:12 -0300

    Update and fix more errors.
    Antes programé todo con SublimeText usando su compilador interno, pero honestamente (y con el debido respeto) apesta.
    Ahora reescribí varias cosas con CodeBlocks, sacando cosas inecesarias que solo añadian complejidad.
    
    Ahora el programa no usa archivos, lee todo de la memoria directa.

--- winter-lib.h
+++ winter-lib.h
@@ -1,62 +1,59 @@
-/* This tool was programed by Joaquin Manuel Crespo (aka Shyan) and are under the GPLv3 */
+/* This tool was created by Joaquin Manuel Crespo (aka, ShyanJMC) and are under the GPL v3.*/
 
-
-#include <stdlib.h>
+/* For support threads*/
+#include <pthread.h>
+/* Standar lib*/
 #include <stdio.h>
+/* SysCalls*/
+#include <stdlib.h>
+/* Strings support*/
 #include <string.h>
-/* Struct for backup names and other resources in the programs that use, */
 
+/* ~/ */
+
+/* Struct for backup names and other resources in the programs that use, */
 struct Backup
 {
 	char name[20];
 	char repository[60];
 	short docker_function;
 	char winter__search_used_or_not;
-	char winter_search_conf[30] = "/etc/winter/wintersearch.conf";
-	char winter_repository_conf[35] = "/etc/winter/winter-repository.conf";
+	char winter_search_conf[30];
+	char *winter_repository_conf[35];
 }Back;
 
 /* Clone the package "a" from the repository in the struct.*/
-void clone(char a[20]){
-	char b[35];
+void clone(char a[10]){
+	char *b = malloc(strlen(Back.repository) + 20);
 	strcpy(b, "git clone ");
 	strcat(b, Back.repository);
+	strcat(b, "/");
 	strcat(b, a);
 	system(b);
 }
 
 /* Open the file that conteins the repository but if don't exist, he create it.*/
-oid repository(){
-	FILE *c;
-	char b[60];
-	c = fopen("/etc/winter/winter-repository.conf" , "r");
-	if (c = NULL ) {
-		fclose(c);
-		char crear[60];
-		strcpy(crear, "touch /etc/winter/winter-repository.conf");
-		system(crear);
-		char repo_shyan[90];
-		strcpy( repo_shyan ,"echo 'https://github.com/ShyanJMC/EkoLinux-Repo/' > /etc/winter/winter-repository.conf");
-		system(repo_shyan);
+/* When read the file that conteins the repository he test if the file can be inside the var.*/
+char *repository(){
+	int eleccion;
+	char *b = malloc(strlen("https://github.com/ShyanJMC/EkoLinux-repo"));
+	printf("\n\n\nElección del repositorio del desarrollador.\n");
+	printf("1- Joaquin Manuel Crespo (Shyan) \n2- Ivan Oudkerk \n\nRepositorio a utilizar:");
+	scanf("%d", &eleccion);
+	switch(eleccion){
+	case (1+0): {strcpy(b, "https://github.com/ShyanJMC/EkoLinux-repo"); break;}
+    case (2+0): {strcpy(b, "https://github.com/ioudkerk/EkoLinux-repo"); break;}
+    default: printf("Por favor ingrese el nº de repo correcto."); break;}
+	return strcpy(Back.repository, b);
+	pthread_exit(NULL);
 	}
-	int i;
-	for (i = 0; feof(c)!= EOF; ++i);
-	if ( i == (60 + 0)){
-	while(!feof(c)){
-		fscanf(c, "%s", &b);}
-	system("clear");
-	strcpy(Back.repository, b);}
-	else { 
-		system("clear");
-		printf("El repositorio supera el stack establecido. \n \b\b\b");
-		printf("Por favor visite la wiki para corregir el repositorio de suite_winter.");
-	}
-}
+
+
 /* Docker function. 1 - instalation 2-run*/
 void docker(short e, char b[60]){
 	char c[60];
 	printf("\n \t Docker initialization \n");
-	if (e = (1 + 0)){
+	if (e == (1 + 0)){
 		Back.docker_function = 1;
 		printf("Building dockerfile, please wait....\n");
 		strcpy(c, "docker build -t ");
@@ -65,12 +62,11 @@ void docker(short e, char b[60]){
 		system(c);
 		printf(" \n Dockerfile installed. If you wish list your images please type: 'docker images' \n");
 	}
-	else if (e = (2 + 0))
+	else if (e == (2 + 0))
 	{
 		Back.docker_function = 2;
 		printf("\n \t Listening docker images: \n");
 		system("docker images");
 		printf("\n \n If you wanna start some of that please type; 'docker run -i -t [IMAGE ID] /bin/bash' \n");
-	} 
+	}
 }
-

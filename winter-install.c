79e547c95312dae1083897aeebeb5fc1b8e55396
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2017-06-22 03:27:52 -0300

    Update and fix errors.
    Cree a repository como un hilo separado del programa principal, corregí unos pequeños errores.

--- winter-install.c
+++ winter-install.c
@@ -1,4 +1,5 @@
-/* This tool was programed by Joaquin Manuel Crespo (aka Shyan) and are under the GPLv3 */
+/* This tool was created by Joaquin Manuel Crespo (aka, ShyanJMC) and are under the GPL v3.*/
+
 
 #include <pthread.h>
 #include <stdio.h>
@@ -7,25 +8,32 @@
 #include "winter-lib.h"
 
 int main (){
-  short a; 
-  char c[30];
-  char b[60];
+  int a;
+  char c[30], b[60];
+  int d;
+  pthread_t hilo_repositorio;
   system("clear");
-  printf("Welcome, Do you wanna: \n 1-Install. \n 2-Run images.\n");
+  printf("Welcome to winter-install, Do you wanna: \n 1-Install. \n 2-Run images.\n");
+  printf("\nSelection: ");
   scanf("%d", &a);
-  repository();
-  printf(" Se esta usando el repositorio: %s \n", Back.repository);
+  pthread_create(&hilo_repositorio, NULL, (void *)repository, NULL);
+  pthread_join(hilo_repositorio, (void *) &d);
+  printf("\n \n Se esta usando el repositorio: %s \n", Back.repository);
   strcpy(b, Back.repository);
   switch(a){
-    case 1: { 
+    case 1: {
   printf("\n Please type the dockerfile to install: " );
-  scanf("%s", &c);
+  scanf("%s", c);
   clone(c);
-  docker(1, c); }
+  docker(1, c);
+  break; }
     case 2: {
       docker(2, c);
+      break;
     }
     default: printf(" Please type a valid option -_- \n");
+    break;
   }
   return 0;
 }
+

445ae158dfc9dcbc7c111d256dd0437ee8e93ef6
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2017-08-20 00:13:06 -0300

    Update winter-terminal.c

--- winter-terminal.c
+++ winter-terminal.c
@@ -1,7 +1,9 @@
-#include <pthread.h> /* hilos y multiprocesamiento */
-#include <stdio.h> /* standar */
-/*#include <stdlib.h>*/ /* for system */
+#include <pthread.h>    /* Posix Threads */
+#include <stdio.h>	/*Standar */
+#include <stdlib.h>     /* Systemcalls and other linux things */
+#include <string.h>     /* Strings*/
 #include "winter-lib.h" /*for all functions */
+#include <time.h>	/*Time support */
 
 
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -12,22 +14,20 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+
 int loop(){
 	char *command;
         while (1 != 0){
-       	command = malloc(300);
-       	if (command == NULL){ return 1;}
+        command = malloc(300);
+        if (command == NULL){ return 1;}
         printf(ANSI_COLOR_GREEN "> " ANSI_COLOR_RESET);
-        fgets(command, 300, stdin);
-       	switch(command[0]){
-       	case ('-'): {command[0] = ' '; system(command); break;}
-       	case ('_'): {return 0; break;}}
-        /* winter-interpreter(command); */ 
-       	free(command);} 
+        fgets(command, 299, stdin);
+        switch(command[0]){
+        case ('-'): {command[0] = ' '; system(command); break;}
+        case ('<'): {return 0; break;}
+        case ('w'): {cache.len_winter_interpreter= strlen(command); winter_interpreter(command); break;}}
+        free(command);}
 }
-
-
-
 
 int main(){
 	system("clear");

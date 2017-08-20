32d317330c28e00b09bb70eda748bb53f985c4a8
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2017-08-20 00:19:53 -0300

    Update winter-lib.h

--- winter-lib.h
+++ winter-lib.h
@@ -17,13 +17,12 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-/* Alias */
+/*Alias */
 #define UDP SOCK_DGRAM
 #define TCP SOCK_STREAM
 
-/* ~/ */
+/*Struct for backup names and other resources in the programs that use */
 
-/* Struct for backup names and other resources in the programs that use, */
 struct Cache
 {
         char name[20];
@@ -32,67 +31,67 @@ struct Cache
         char winter__search_used_or_not;
         char winter_search_conf[30];
         char *winter_repository_conf[35];
-        char socket_protocol;
+        char socket_protocol; /* U = UDP T = TCP */
         char ip_adress[15];
-        int socket_port;
-        int socket_file_descriptor;
 	int is_root; /* 0 for yes, 1 for no */
+	int len_winter_interpreter;
 }cache;
 
-int check_user(){ 
-	if (geteuid() ) { printf("Using root privilegies");return (cache.is_root = 0); }
+int check_user(){
+	if ( geteuid() ) { printf("Using root privilegies");return (cache.is_root = 0); }
+	printf("You are not root");
 	return (cache.is_root = 1);
 }
 
-/* In future versiones I will atach support for RAW sockets.*/
+/* In future versions I will atach support for RAW sockets.*/
 
 int socket_client(char protocol, const char ip[15] ){
-        /* Struct for sockets and their bridge vars*/
-        struct sockaddr *sock_bridge;
-        struct addrinfo *hints;
-        struct addrinfo **res;
-        int a, send_file, recv_file, msg_len;
-        char buffer[7000], port[2];
-        printf("Socket function initializated \n");
-        printf("Insert port: ");
-        scanf("%s", port);
+	/* Struct for sockets and their bridge vars*/
+	struct sockaddr *sock_bridge;
+	struct addrinfo *hints;
+	struct addrinfo **res;
+	int a, send_file, recv_file, msg_len;
+	char buffer[7000], port[2];
+	printf("Socket function initializated \n");
+	printf("Insert port: ");
+	scanf("%s", port);
         getaddrinfo(ip, port, hints, res);
-        if (protocol == 'U'){ a = socket(PF_INET, SOCK_DGRAM, 0); }
-        else { a = socket(PF_INET, SOCK_STREAM, 0); }
+	if (protocol == 'U'){ a = socket(PF_INET, SOCK_DGRAM, 0); }
+	else { a = socket(PF_INET, SOCK_STREAM, 0); }
         if (a == -1) { printf("Error in socket"); return 1; }
         printf("Please insert the messege to send to target: \n");
         scanf("%s", buffer);
         printf("Connecting to target and sending messege\n");
         connect(a, sock_bridge, 15);
         if (a == -1){
-                    	printf("Error in socket\n");
-                        return 1;}
+			printf("Error in socket\n");
+			return 1;}
         msg_len = strlen(buffer);
         send_file = send(a, buffer, msg_len, 0);
         if (send_file == -1){
-                        printf("error in sending \n");
-                        return 1; }
+			printf("error in sending \n");
+			return 1; }
         printf("Waiting answer ...\n");
         recv_file = recv(a, buffer, 7000, 0);
         switch(recv_file){
                 case(-1+0):
-                        printf("Error in recv \n");
-                        break;
+			printf("Error in recv \n");
+			break;
                 case(0+0):
-                        printf("The target close the conecttion \n");
-                        break;
+			printf("The target close the conecttion \n");
+			break;
                 default:
-                        printf("%s \n", buffer);
-                        break;}
-        return 0;
+			printf("%s \n", buffer);
+			break;}
+	return 0;
         pthread_exit(NULL); }
 
 
 /* Clone the package "a" from the repository in the struct.*/
 void clone(char a[10]){
-	char *b = malloc(strlen(Back.repository) + 20);
+	char *b = malloc(strlen(cache.repository) + 20);
 	strcpy(b, "git clone ");
-	strcat(b, Back.repository);
+	strcat(b, cache.repository);
 	strcat(b, "/");
 	strcat(b, a);
 	system(b);
@@ -108,7 +107,7 @@ char *repository(){
 	case (1+0): {strcpy(b, "https://github.com/ShyanJMC/EkoLinux-repo"); break;}
     case (2+0): {strcpy(b, "https://github.com/ioudkerk/EkoLinux-repo"); break;}
     default: printf("Por favor ingrese el nº de repo correcto."); break;}
-	return strcpy(Back.repository, b);
+	return strcpy(cache.repository, b);
 	pthread_exit(NULL);
 	}
 
@@ -130,7 +129,7 @@ void docker(short e, char b[60]){
 	char c[60];
 	switch(e){
 	case (1 +0):
-	    Back.docker_function = 1;
+	    cache.docker_function = 1;
 		printf("Building dockerfile, please wait....\n");
 		strcpy(c, "docker build -t ");
 		strcat(c, b);
@@ -139,11 +138,35 @@ void docker(short e, char b[60]){
 		printf(" \n Dockerfile installed. If you wish list your images please type: 'docker images' \n");
 		break;
     case (2 + 0):
-        Back.docker_function = 2;
+        cache.docker_function = 2;
 		printf("\n \t Listening docker images: \n");
 		system("docker images");
 		printf("\n \n If you wanna start some of that please type; 'docker run -i -t [IMAGE ID] /bin/bash' \n");
 		break;
 	}
 
+}
+
+/* Winter Interpreter */
+int winter_interpreter(char comando[300]){
+	int temporal1;
+	char temporal2[60];
+        if ((strcmp( comando, "wdocker"))== 0){
+			printf("\n 1-Install 2-Run images");
+			scanf("%d", &temporal1);
+			printf("\n Insert the name of dockerfile to install");
+			fgets(temporal2, 60, stdin);
+			docker(temporal1, temporal2);
+			return 0; }
+	else if((strcmp( comando, "wrepo"))== 0){
+			repository();
+			return 0; }
+	else if((strcmp( comando, "wcheck")) == 0){
+			check_user();
+			return 0; }
+	else if((strcmp( comando, "weko")) == 0){
+			repository();
+			return 0; }
+	else {printf("Winter command not recognized \n");return 0;}
+        /*pthread_exit(NULL);*/
 }
